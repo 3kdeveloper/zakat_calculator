ee9b8a660c88b1543dfb497affc62e371f2452b4
kamran khan <77343380+3kdeveloper@users.noreply.github.com>
2022-03-12 17:47:54 +0500

    adding colors

--- lib/utils/color.dart
+++ lib/utils/color.dart
@@ -1,6 +1,9 @@
 import 'exports.dart';
 
 class AppColors {
-  static const kPrimaryColor = Color(0xFF228B22);
-  static const kAccentColor = Color(0xFFffffff);
+  static const kPrimaryColor = Color(0xBF0D7F0D);
+  static const kAccentColor = Color(0xFF97C690);
+  static const kWhiteColor = Color(0xFFffffff);
+  static const kBlackColor = Color(0xff000000);
+  static const kTextFieldColor = Color(0xffe7f8e4);
 }
